49b89a05b2e5993fe9a8d74d07cf1cf9bf1dd11b
Dineshpolo <46916594+Dineshpolo@users.noreply.github.com>
2022-07-07 14:39:57 +0530

    versions.tf

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.53, < 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 3.53, < 5.0"
+    }
+  }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/cloudrun/v0.0.1"
+  }
